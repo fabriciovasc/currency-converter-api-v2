ccdc77525728ada960c0b744dfddc1abe3519af8
Fabrício Vasconcellos <fabricio.vasconc@gmail.com>
2023-02-19 23:21:23 -0300

    fix(HttpModule): Add module inside app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import validationConfig from './config/validation-config';
 import appConfig from './config/app-config';
+import { HttpModule } from './infra/http/http.module';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import appConfig from './config/app-config';
       validationSchema: validationConfig,
       load: [appConfig],
     }),
+    HttpModule,
   ],
   controllers: [],
   providers: [],
